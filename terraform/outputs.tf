45a58b99396d985fb97d3ea6e156aa43ea81c223
mukuzero <mukunthan@protonmail.ch>
2025-12-27 18:59:42 +0530

    testing end to end and invoke automatically

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "db_connection_name" {
   description = "The connection name of the Cloud SQL instance"
   value       = google_sql_database_instance.default.connection_name
 }
+
+output "function_uri" {
+  description = "The URI of the Cloud Run Function"
+  value       = google_cloudfunctions2_function.data_loader.service_config[0].uri
+}
